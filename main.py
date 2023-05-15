acb0f9bf0f1f0c625cea1eefac2ce8cbfc37ec35
darwin <darwindelacruz0012@gmail.com>
2023-05-15 16:10:20 +0800

    Implement the AboutDialog class

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
 
         search_action = QAction(QIcon("icons/icons/search.png"), "Search", self)
         search_action.triggered.connect(self.search)
@@ -89,7 +90,19 @@ class MainWindow(QMainWindow):
         delete_dialog = DeleteDialog()
         delete_dialog.exec()
 
+    def about(self):
+        about_dialog = AboutDialog()
+        about_dialog.exec()
 
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        This app was created for learning purposes
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
